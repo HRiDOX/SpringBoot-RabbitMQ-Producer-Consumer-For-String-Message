8cd3b3355529397ec8a3290178b8b16eed988635
HRiDOX <rgrrf123@gmail.com>
2023-04-05 15:31:07 +0600

    RabbitMQ String Messgae

---
+++ springboot-rabbitmq/src/main/java/net/javaguides/springbootrabbitmq/publisher/RabbitMQProducer.java
@@ -0,0 +1,33 @@
+package net.javaguides.springbootrabbitmq.publisher;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/*Well, once we annotate this class with @Service annotation, then this @Service annotation will
+
+create this class as a spring bean and it will register in spring IOC container.*/
+@Service
+public class RabbitMQProducer {
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQProducer.class);
+
+    public RabbitTemplate rabbitTemplate;
+
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message){
+        LOGGER.info(String.format("Message sent => %s",message));
+        rabbitTemplate.convertAndSend(exchange,routingKey,message);
+    }
+}
